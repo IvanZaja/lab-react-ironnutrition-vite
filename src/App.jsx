04d1a3bd574ad5fa7d2a6bb9287267490336d00a
Ivan Zajaczkowski <sayonbra@gmail.com>
2024-04-07 13:53:24 +0200

    Feat: impoerted card from antd, successfully showing the food list from de JSon, delete button implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import "./App.css";
+import FoodBox from "./components/FoodBox";
+import foodsJson from "./foods.json";
+
+import { useState } from "react";
 
 function App() {
+  const [foodList, setFoodList] = useState(foodsJson);
+
+  const deleteFood = (food) => {
+    setFoodList([...foodList.filter((f) => f.id !== food.id)])
+  }
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <FoodBox foodList={ foodList } deleteFood={ deleteFood } />
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,22 @@
-// Your code here
+import { Card } from "antd";
+import { DeleteOutlined } from '@ant-design/icons';
+const { Meta } = Card;
+
+function FoodBox({ foodList, deleteFood }) {
+  return (
+    <div>
+        {foodList.map(food => (
+            <div style={{margin:'10px'}} key={food.id}>
+                <Card style={{ width: 300}} cover={<img alt="example" src={food.image} />} actions={[ <DeleteOutlined onClick={() => deleteFood(food)} key="delete" /> ]}>
+                    <Meta title={food.name} description={`Calories: ${food.calories}`}/>
+                    <p>Servings: {food.servings}</p>
+                    <p><b>Total Calories: {food.servings * food.calories}</b> kcal</p>
+                </Card>
+            </div>
+        ))}
+    </div>
+    
+  )
+}
+
+export default FoodBox;
